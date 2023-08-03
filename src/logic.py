7a048deaf0fa0fff6eedde50499954fd49a4a07f
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-08-03 14:29:34 +0300

    Capture enemy units when landing on them

--- src/logic.py
+++ src/logic.py
@@ -12,7 +12,11 @@ UNIT_COUNT = 4
 DICE_COUNT = 1
 ROLL_MIN = 1
 ROLL_MAX = 6
+_AVG_ROLL = (ROLL_MIN + ROLL_MAX) / 2
 STARTING_POSITIONS = tuple(BOARD_SIZE * i for i in range(PLAYER_COUNT))
+INITIAL_ORDER_OFFSETS = tuple(
+    round(_AVG_ROLL * i / PLAYER_COUNT) for i in range(PLAYER_COUNT)
+)
 
 
 class Track(Enum):
@@ -24,6 +28,7 @@ class Track(Enum):
 @dataclass_json
 @dataclass
 class Unit:
+    index: int
     position: int = 0
     track: Track = Track.START
 
@@ -36,7 +41,7 @@ class Unit:
 class Player:
     index: int
     units: list[Unit] = field(
-        default_factory=lambda: [Unit() for i in range(UNIT_COUNT)]
+        default_factory=lambda: [Unit(i) for i in range(UNIT_COUNT)]
     )
     dice: list[int] = field(
         default_factory=lambda: [i + ROLL_MIN for i in range(DICE_COUNT - 1)]
@@ -49,7 +54,7 @@ class Player:
 @dataclass_json
 @dataclass
 class GameState:
-    turn: int = -1
+    turn: int = 0
     players: list[Player] = field(
         default_factory=lambda: [Player(i) for i in range(PLAYER_COUNT)]
     )
@@ -57,7 +62,12 @@ class GameState:
     @classmethod
     def new_game(cls) -> "GameState":
         game = cls()
-        game._iterate_turn()
+        # Start all units
+        for player in game.players:
+            unit = player.units[0]
+            unit.track = Track.MAIN
+            offset = INITIAL_ORDER_OFFSETS[player.index]
+            unit.position = STARTING_POSITIONS[player.index] + offset
         return game
 
     def __hash__(self) -> int:
@@ -87,33 +97,54 @@ class GameState:
         # Resolve input
         unit = player.units[unit_index]
         die_value = player.dice[die_index]
-        # Resolve result based on unit track and die
         if unit.track == Track.END:
             return "Units that finished cannot move"
         elif unit.track == Track.START:
-            # Unit can only leave spawn when rolling the minimum
             if die_value != ROLL_MIN:
                 return f"Units in spawn can only move with a {ROLL_MIN}"
+            player.dice.pop(die_index)
             unit.track = Track.MAIN
             unit.position = STARTING_POSITIONS[player.index]
-            player.dice.pop(die_index)
             return f"Moved on to track (#{die_index})"
         else:
             # Units on track can always use any die
             assert unit.track == Track.MAIN
-            unit.position += die_value
             player.dice.pop(die_index)
-            # TODO implement unit capture
-            self._iterate_turn()
-            return f"Moved +{die_value} (#{die_index})"
+            unit.position += die_value
+            response = f"Moved +{die_value} (#{die_index})"
+            captured = self._capture(player.index, unit.position)
+            if captured:
+                captured_repr = " ".join([f"({p + 1},{u + 1})" for p, u in captured])
+                response = f"{response}, captured {captured_repr}"
+            turn_over = die_value != ROLL_MAX and not captured
+            if turn_over:
+                self.turn += 1
+            return response
 
-    def _iterate_turn(self):
-        self.turn += 1
-        # Move one unit to track if no units are on track
-        player = self.get_player()
-        if all(unit.track != Track.MAIN for unit in player.units):
+    def _capture(
+        self,
+        player_index: int,
+        capture_position: int,
+    ) -> list[tuple[int, int]]:
+        capture_position %= TRACK_SIZE
+        if capture_position in STARTING_POSITIONS:
+            return []
+        captured = []
+        for player in self.players:
+            if player.index == player_index:
+                # Ignore friendly units
+                continue
             for unit in player.units:
-                if unit.track == Track.START:
-                    unit.track = Track.MAIN
-                    unit.position = STARTING_POSITIONS[player.index]
-                    break
+                if capture_position == unit.position % TRACK_SIZE:
+                    unit.track = Track.START
+                    # Capture
+                    captured.append((player.index, unit.index))
+            if all(unit.track != Track.MAIN for unit in player.units):
+            # Leave at least one unit per player on the track
+                for unit in player.units:
+                    if unit.track == Track.START:
+                        unit.track = Track.MAIN
+                        unit.position = STARTING_POSITIONS[player.index]
+                        # Rescue
+                        break
+        return captured
